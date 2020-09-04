c15ceeb2a580c5dc48aea74e866fc74ca208a899
Andreea Ros <ros.andreeacatalina@gmail.com>
2020-09-04 12:15:10 +0100

    Initial commit.

---
+++ app.js
@@ -0,0 +1,50 @@
+const createError = require("http-errors");
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+const router = express.Router();
+const indexRouter = require("./routes/index");
+
+const app = express();
+
+// initialise database
+const mongoose = require("mongoose");
+const PropertyListingModel = require("./models/property");
+
+const mongoDB =
+  "mongodb+srv://admin:admin@cluster0.zzqj7.mongodb.net/surreal-estate?retryWrites=true&w=majority";
+mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
+
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use("/api/v2", indexRouter);
+
+// catch 404 and forward to error handler
+app.use((req, res, next) => {
+  next(createError(404));
+});
+
+// error handler
+app.use((err, req, res, next) => {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.send("error");
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Surreal Estate API is running on :${PORT}`);
+});
+
+module.exports = app;
---
+++ models/favourite.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const FavouriteSchema = new mongoose.Schema({
+  fbUserId: { type: String, require: true },
+  propertyListing: {
+    type: Schema.Types.ObjectId,
+    ref: "PropertyListing",
+    require: true,
+  },
+});
+
+const FavouriteModel = mongoose.model("Favourite", FavouriteSchema);
+
+module.exports = FavouriteModel;
