6df08e9efa67ec71680082f5c9ec2c54d217fa42
Andreea Ros <ros.andreeacatalina@gmail.com>
2020-09-04 16:20:42 +0100

    Set up basic GET and POST functionality.

--- app.js
+++ app.js
@@ -1,50 +1,53 @@
-const createError = require("http-errors");
 const express = require("express");
-const path = require("path");
-const cookieParser = require("cookie-parser");
-const logger = require("morgan");
+const { MongoClient } = require("mongodb");
+const bodyParser = require("body-parser");
 const router = express.Router();
-const indexRouter = require("./routes/index");
+
+require("dotenv").config();
+const PORT = process.env.PORT || 3000;
+const mongoDB = process.env.URL;
 
 const app = express();
+let db, testCollection;
+
+const jsonParser = bodyParser.json();
+app.use(jsonParser);
 
 // initialise database
-const mongoose = require("mongoose");
-const PropertyListingModel = require("./models/property");
+MongoClient.connect(mongoDB, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+  .then((client) => {
+    console.log("You have connected to the database");
+    db = client.db("surreal-estate");
+    testCollection = db.collection("test");
 
-const mongoDB =
-  "mongodb+srv://admin:admin@cluster0.zzqj7.mongodb.net/surreal-estate?retryWrites=true&w=majority";
-mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
-const db = mongoose.connection;
-db.on("error", console.error.bind(console, "MongoDB connection error:"));
+    // start app after database connection is initialized
+    app.listen(PORT, () => {
+      console.log(`Surreal Estate API is running on :${PORT}`);
+    });
+  })
+  .catch((err) => console.log(err));
 
-app.use(logger("dev"));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
+// test GET request to "/"
+app.get("/", (req, res) => res.status(200).json({ hello: "world" }));
 
-app.use("/api/v2", indexRouter);
-
-// catch 404 and forward to error handler
-app.use((req, res, next) => {
-  next(createError(404));
+// test POST and GET to "/test"
+app.post("/test", (req, res) => {
+  testCollection
+    .insertOne(req.body)
+    .then((result) => res.status(201).json(result.ops))
+    .catch((error) => console.log(error));
 });
 
-// error handler
-app.use((err, req, res, next) => {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.send("error");
-});
-
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Surreal Estate API is running on :${PORT}`);
+app.get("/test", (req, res) => {
+  db.collection("test")
+    .find()
+    .toArray()
+    .then((results) => {
+      res.status(200).json(results);
+    });
 });
 
 module.exports = app;
--- routes/index.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-/* GET home page. */
-router.route("/").get((req, res, next) => {
-  res.status(200).json({ hello: "world" });
-});
-
-module.exports = router;
