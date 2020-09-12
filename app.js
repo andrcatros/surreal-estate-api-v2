822a0cb9a0ddd55e0d46f9e55b6efa96fd5fa334
Andreea Ros <ros.andreeacatalina@gmail.com>
2020-09-12 14:02:32 +0100

    Test implementation of fetching images from GridFS.

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const cors = require("cors");
 
 const multer = require("multer");
 const GridFsStorage = require("multer-gridfs-storage");
+const Grid = require("gridfs-stream");
 const crypto = require("crypto");
 const path = require("path");
 
@@ -43,9 +44,7 @@ db.once("open", () => {
   });
 
   // initialise GFS
-  const gfs = new mongoose.mongo.GridFSBucket(db.db);
-  const imgCollection = db.collection("imgs.chunks");
-  const imgFiles = db.collection("imgs.files");
+  /*const gfs = new mongoose.mongo.GridFSBucket(db.db); */
 
   const storage = new GridFsStorage({
     url: mongoURL,
@@ -67,20 +66,31 @@ db.once("open", () => {
   });
 
   const upload = multer({ storage });
+  const gfs = Grid(db.db, mongoose.mongo);
 
   app.use("/api/v2/PropertyListing", PropertyRouter);
   app.use("/api/v2/Favourite", FavouriteRouter);
+
   app.post("/upload", upload.single("img"), PropertyController.create);
   app.get("/upload", (req, res) => {
-    db.collection("fs.files")
-      .find()
-      .toArray((err, files) => {
-        if (!files) {
-          return res.status(404).json({ err: "No files exist" });
+    db.collection("fs.files").findOne(
+      { filename: req.body.filename },
+      (err, file) => {
+        if (!file || file.length === 0) {
+          return status(404).json({ err: "No File Found" });
+        } else {
+          if (
+            file.contentType === "image/jpeg" ||
+            file.contentType === "image/png"
+          ) {
+            const readstream = gfs.createReadStream(file.filename);
+            readstream.pipe(res);
+          } else {
+            res.status(404).json({ err: "File is not an image" });
+          }
         }
-
-        res.status(200).json(files);
-      });
+      }
+    );
   });
 });
 
