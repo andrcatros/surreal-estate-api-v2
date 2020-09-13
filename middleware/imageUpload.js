df7cb202210d4a7aafebec8d54090c9974f0b1d3
Andreea Ros <ros.andreeacatalina@gmail.com>
2020-09-13 17:48:08 +0100

    Added file size and type restrictions to imageUpload.

--- middleware/imageUpload.js
+++ middleware/imageUpload.js
@@ -13,6 +13,16 @@ const storage = multer.diskStorage({
   },
 });
 
-const upload = multer({ storage: storage });
+const upload = multer({
+  storage: storage,
+  fileFilter: (req, file, callback) => {
+    var ext = path.extname(file.originalname);
+    if (ext !== ".png" && ext !== ".jpg" && ext !== ".gif" && ext !== ".jpeg") {
+      return callback(new Error("Only images are allowed"));
+    }
+    callback(null, true);
+  },
+  limits: { fileSize: 50000 },
+});
 
 module.exports = upload;
